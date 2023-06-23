24915cec4d9e452546468de228aedfefeaa87c78
Brennan Phifer <bman5803@gmail.com>
2023-06-22 21:14:49 -0600

    Created Models with Vacation Controller

---
+++ VacationQuestsAPI/Model/EventModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Cosmos.Serialization.HybridRow;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace VacationQuestsAPI.Model
+{
+    
+    public class EventModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string EventName { get; set; }
+        public string Location { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<UserModel> Party { get; set; }
+        public int PartyCount { get; set; }
+    }
+}
---
+++ VacationQuestsAPI/Model/VacationModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+namespace VacationQuestsAPI.Model
+{
+    public class VacationModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string VacationTitle { get; set; }
+        public IEnumerable<EventModel>? Events { get; set; }
+        public IEnumerable<UserModel>? Planners { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
