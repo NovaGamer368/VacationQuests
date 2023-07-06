82d0625d62e335a8469f3b27ce5bfd86bf080e56
Brennan Phifer <bman5803@gmail.com>
2023-07-06 16:53:13 -0600

    Vacation Successfully Created and stored

--- VacationQuests/ClientApp/src/components/vacation/CreateVacation.js
+++ VacationQuests/ClientApp/src/components/vacation/CreateVacation.js
@@ -2,103 +2,64 @@
 import Cookies from 'js-cookie';
 import LocationSelector from './LocationSelector';
 import { Form } from 'react-bootstrap';
+import DateSelection from './DateSelection';
 
 const CreateVacation = () => {
     const [location, setLocation] = useState('')
     const [locationFound, setLocationFound] = useState(false)
-
-    const [numberOfDays, setNumberOfDays] = useState(0)
-    const [daysSet, setDaysSet] = useState(false)
-
-    const [startDate, setStartDate] = useState(null)
-    const [endDate, setEndDate] = useState(null)
-
+    const [currentUser, setCurrentUser] = useState(null)
 
     useEffect(() => {
-       
+        fetch(`https://localhost:7259/api/users/${Cookies.get("UserId")}`)
+            .then(resp => resp.json())
+            .then(data => {
+                setCurrentUser(data)
+            })
+            .catch(e => console.log(e))
     }, [])
 
-    const submitDays = () => {
-        setDaysSet(true)
-    }
+    const createVacation = (startDate, endDate) => {
+        //Check if someone is actually logged in
+        if (currentUser !== null) {
+            console.log('is logged in')
+            // Check if location, startDate, and endate variables are there
+            if (location != '') {
+                console.log("Creating Vacation")
+                //Create the vacation
+                const requestOptions = {
+                    mode: 'cors',
+                    method: 'Post',
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ VacationTitle: location + ' Vacation', StartDate: startDate, EndDate: endDate, planners: [currentUser]}),
+                    origin: "https://localhost:44455"
+                };
 
-    const setDates = (e) => {
-        const inputDate = new Date(e);
-        setStartDate(inputDate)
-
-        const increasedDate = new Date(inputDate.setDate(inputDate.getDate() + numberOfDays));
-        setEndDate(increasedDate.toISOString().split("T")[0]);
-
-        //setEndDate(startDate.getDate() + numberOfDays)
-        //console.log(endDate)
-    }
-
-    const calculateNewDate = () => {
-        const selectedDateObj = new Date(startDate);
-        const newDateObj = new Date(selectedDateObj.getTime() + numberOfDays * 24 * 60 * 60 * 1000);
-        const newDateStr = newDateObj.toISOString().slice(0, 10);
-        setEndDate (newDateStr);
-    };
-
-    if (locationFound)
-    {
-        if (daysSet) {
-            return (
-                <>
-                    <div className='d-flex justify-content-center flex-column'>
-                        <h1>Collecting Start Date of Vacation</h1> 
-                        <div className='d-flex flex-column my-5'>
-                            <div className=" md-form md-outline input-with-post-icon datepicker">
-                                <label  htmlFor="startDate">Select a date:</label>
-                                <input
-                                    className="form-control"
-                                    type="date"
-                                    id="startDate"
-                                    value={startDate}
-                                    onChange={(e) => { setStartDate(e.target.value) } }
-                                />
-                            </div>
-                            <button className='btn btn-primary m-5' onClick={calculateNewDate}>Get Date {numberOfDays} Days Later</button>
-                            {endDate && <p className='text-center'>The dates of the vacation would be from {startDate} till {endDate}.</p>}
-                        </div>
-                    </div>
-                </>
-            )
+                fetch("https://localhost:7259/api/vacations", requestOptions)
+                    .then(resp => resp.json())
+                    .then(data => {
+                        window.location.href = '/'
+                    })
+                    .catch(e => console.log(e))
+            }
         }
         else {
-
-            return (
-                <>
-                    <div>
-                        <button className='btn btn-secondary h-auto m-3' onClick={() => { setLocationFound(false) } }>
-                            <i class="bi bi-arrow-90deg-left"></i> 
-                        </button>
-                        <div className='d-flex justify-content-center align-items-center flex-column'>
-                            <h1>Creating Vacation to {location}</h1> 
-                            <div>
-                                <div>How many days will you be visiting {location}</div>
-                                <div class="input-group mb-3 w-75">
-                                    <input type="text" class="form-control" placeholder="Number of days"
-                                        onKeyPress={(event) => {
-                                            if (!/[0-9]/.test(event.key)) {
-                                                event.preventDefault();
-                                            }
-                                        }}
-                                        onChange={(e) => { setNumberOfDays(e.target.value); console.log(numberOfDays) }}      
-                                    />
-                                    <button class="btn btn-primary" onClick={() => { submitDays(); } }>Submit</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            );
+            console.log('Failed')
         }
     }
-    else
-    {
+
+    if (locationFound) {
         return (
-            <>                
+            <>
+                <DateSelection location={location} clearLocationFound={setLocationFound} createVacation={createVacation} />
+            </>
+        )
+    }
+    else {
+        return (
+            <>
                 <div className='text-center'>
                     <h1>Creating Vacation</h1>
                     <LocationSelector location={setLocation} locationFound={setLocationFound} />
---
+++ VacationQuests/ClientApp/src/components/vacation/DateSelection.js
@@ -0,0 +1,94 @@
+﻿import React, { useEffect, useState } from 'react';
+
+const DateSelection = ({ location, clearLocationFound, createVacation }) => {
+    const [numberOfDays, setNumberOfDays] = useState(0)
+    const [daysSet, setDaysSet] = useState(false)
+
+    const [startDate, setStartDate] = useState(null)
+    const [endDate, setEndDate] = useState(null)
+
+    const [errorMsg, setErrorMsg] = useState('')
+
+    useEffect(() => {
+        calculateNewDate()
+    }, [startDate])
+
+    const submitDays = () => {
+        setDaysSet(true)
+    }
+
+    const calculateNewDate = () => {
+        const selectedDateObj = new Date(startDate);
+        const newDateObj = new Date(selectedDateObj.getTime() + numberOfDays * 24 * 60 * 60 * 1000);
+        const newDateStr = newDateObj.toISOString().slice(0, 10);
+        setEndDate(newDateStr);
+    };
+
+    const validateFilled = () => {
+        if (startDate != null && endDate != null) {
+            createVacation(startDate, endDate)
+        }
+        else { setErrorMsg('Please select a start date') }
+    }
+
+    if (daysSet) {
+        return (
+            <>
+                <div>
+                    <button className='btn btn-secondary h-auto m-3' onClick={() => { setDaysSet(false) }}>
+                        <i class="bi bi-arrow-90deg-left"></i>
+                    </button>
+                    <div className='d-flex justify-content-center flex-column'>
+                        <h1>Collecting Start Date of Vacation</h1>
+                        <div className='d-flex flex-column my-5'>
+                            <div className=" md-form md-outline input-with-post-icon datepicker">
+                                <label htmlFor="startDate">Select a date:</label>
+                                <input
+                                    className="form-control"
+                                    type="date"
+                                    id="startDate"
+                                    value={startDate}
+                                    onChange={(e) => { setStartDate(e.target.value) }}
+                                />
+                            </div>
+                            {endDate && <p className='text-center'>The dates of the vacation would be from {startDate} till {endDate}.</p>}
+                            <div className='text-danger text-center'>{errorMsg}</div>
+                            <button className='btn btn-primary m-5' onClick={validateFilled}>Confirm dates</button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+    else {
+
+        return (
+            <>
+                <div>
+                    <button className='btn btn-secondary h-auto m-3' onClick={() => { clearLocationFound(false) }}>
+                        <i class="bi bi-arrow-90deg-left"></i>
+                    </button>
+                    <div className='d-flex justify-content-center align-items-center flex-column'>
+                        <h1>Creating Vacation to {location}</h1>
+                        <div>
+                            <div>How many days will you be visiting {location}</div>
+                            <div class="input-group mb-3 w-75">
+                                <input type="text" class="form-control" placeholder="Number of days"
+                                    onKeyPress={(event) => {
+                                        if (!/[0-9]/.test(event.key)) {
+                                            event.preventDefault();
+                                        }
+                                    }}
+                                    onChange={(e) => { setNumberOfDays(e.target.value)}}
+                                />
+                                <button class="btn btn-primary" onClick={() => { submitDays(); }}>Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+};
+
+export default DateSelection; 
