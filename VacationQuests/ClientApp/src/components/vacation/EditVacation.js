e0a04ce18ebcd72c4ac022f3f5f5efb3bd4e99b3
Brennan Phifer <bman5803@gmail.com>
2023-07-10 15:39:19 -0600

    EditVacation Dates setup and showing properly

--- VacationQuests/ClientApp/src/components/vacation/EditVacation.js
+++ VacationQuests/ClientApp/src/components/vacation/EditVacation.js
@@ -1,30 +1,104 @@
 ﻿import React, { useEffect, useState } from 'react';
-import GoogleCalendar from '../GoogleCalendar.js'
+import moment from 'moment'
+
 
 const EditVacation = () => {
     const queryParams = new URLSearchParams(window.location.search)
-    const [vacation, setVacation] = useState(null)
+    const [vacation, setVacation] = useState()
+    const [startDate, setStartDate] = useState()
+    const [endDate, setEndDate] = useState()
+    const [dates, setDates] = useState([]);
 
+    //On start
     useEffect(() => {
+        getVacation();
+    }, [])
+
+    //Vacation Changes
+    useEffect(() => {
+        console.log('vacation ', vacation)
+        if (vacation) {
+            initDates()
+            if (vacation.events) {
+                getEvents();
+            }
+        }
+    }, [vacation])
+
+    //Gets vacation through ID passed in param
+    const getVacation = async () => {
         for (const [key, value] of queryParams) {
-            if (key == 'v')
-            {
-                fetch(`https://localhost:7259/api/vacations/${value}`)
+            if (key == 'v') {
+                await fetch(`https://localhost:7259/api/vacations/${value}`)
                     .then(resp => resp.json())
                     .then(data => {
                         setVacation(data)
+                        setStartDate(data.startDate)
+                        setEndDate(data.endDate)
                     })
                     .catch(e => console.log(e))
             }
+        }       
+    }
+    //Matching events for this vacation
+    const getEvents = () => {
+        fetch(`https://localhost:7259/api/events`)
+            .then(resp => resp.json())
+            .then(data => {
+                data.forEach((event) => {
+                    console.log('vacation event id', vacation.event.id)
+                    if (vacation.event.id.includes(event.id)) {
+                        console.log('FOUND EVENT')
+                    }
+                })
+            })
+            .catch(e => console.log(e))
+    }
+
+    //Sets the dates according to how many days the vacation has been planned for
+    const initDates = () => {
+        let loop = true;
+        let date = new Date(startDate);
+        const end = new Date(endDate)
+        let dateList = []
+
+        while (loop) {
+            if (date.toISOString().slice(0, 10) == end.toISOString().slice(0, 10)) {
+                loop = false
+            }
+            dateList.push(date)
+            date = new Date(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+            
         }
+        console.log('date list data: ', dateList)
+        setDates(dateList)
+    }
 
-    }, [])
+    //Object rendering on the fron end
+    if (vacation) {
+        return (
+            <div className='text-center' >
+                <h1>Dates of Vacation</h1>
+                <br/><hr/>
+                <div className='d-flex'>
+                    {
+                        dates.map((date) => (
+                            <div className='card border-secondary m-3'>
+                                <h2>{moment(date).format('MMMM Do YYYY')}</h2>
+                                <hr />
+                                <h4>Events</h4>
 
-    return (
-        <>
-            <GoogleCalendar />
-        </>
-    );
+                        </div>
+                        ))  
+                            
+                    }
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (<div>Loading Vacation   </div>)
+    }
 };
 
 export default EditVacation; 
