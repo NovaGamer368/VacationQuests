3012c2d1899804e210d62de79ead430874a6a816
Brennan Phifer <bman5803@gmail.com>
2023-07-20 15:50:11 -0600

    Got Filtered Locations

--- VacationQuests/ClientApp/src/components/vacation/LocationSelector.js
+++ VacationQuests/ClientApp/src/components/vacation/LocationSelector.js
@@ -1,6 +1,13 @@
 ﻿import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import SplitButton from 'react-bootstrap/SplitButton';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
 
 const LocationSelector = ({ location, locationFound }) => {
 
@@ -9,16 +16,23 @@ const LocationSelector = ({ location, locationFound }) => {
     const [error, setError] = useState('')
 
     useEffect(() => {
-        let api_url = 'https://restcountries.com/v3.1/all?fields=name'
+        //let api_url = 'https://restcountries.com/v3.1/all?fields=name'
+        let api_url = 'https://restcountries.com/v3.1/all?'
 
         fetch(api_url)
             .then(resp => resp.json())
             .then(data => {
                 //SORT DATA RETURNED
-                setCountries(data)
+                console.log('data: ', data)
+                sortCountriesByName(data)
             })
             .catch(e => console.log(e))
     }, []);
+
+    //useEffect(() => {
+    //    if (countries) {
+    //    }
+    //}, [countries]);
 
     const selectCountry = () => {
         if (selected != 'Select a country') {
@@ -29,24 +43,80 @@ const LocationSelector = ({ location, locationFound }) => {
             setError('Must Select a country')
         }
     }
-    
+
+    const sortCountriesByName = (data) => {
+        const sortedCountries = [...data].sort((a, b) => {
+            const nameA = a.name.common.toUpperCase();
+            const nameB = b.name.common.toUpperCase();
+            if (nameA < nameB) return -1;
+            if (nameA > nameB) return 1;
+            return 0;
+        });
+
+        setCountries(sortedCountries);
+        console.log(sortedCountries)
+    };
+
     return (
         <>
             <div className='d-flex flex-column justify-content-center align-items-center'>
-                <h3 className='text-danger'>{ error }</h3>
-                <div className='mt-5 w-100'>
-                    <Dropdown>
-                        <Dropdown.Toggle className='col-4' variant="secondary" id="dropdown-basic">
-                            { selected }
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>                            {
-                                countries.map((country) => (
-                                    <Dropdown.Item onClick={() => { setSelected(country.name.common)}}> {country.name.common}</Dropdown.Item>
-                                ))                            }
-                        </Dropdown.Menu>
-                    </Dropdown>
+                <h3 className='text-danger'>{error}</h3>
+                <div className='d-flex justify-content-center mt-5 w-100'>
+                    {/*<Dropdown className='col-5 mx-auto'>*/}
+                    {/*    <DropdownButton*/}
+                    {/*        drop={'down-centered'}*/}
+                    {/*        variant="secondary"*/}
+                    {/*        title={selected}*/}
+                    {/*    >*/}
+                    {/*        <Dropdown.Item>*/}
+                    {/*            <div className='wrapper'>*/}
+                    {/*                <input type="text" className="form-control input" placeholder="Search for Country" />*/}
+                    {/*                <i class="bi bi-search icon"></i>*/}
+                    {/*            </div>*/}
+                    {/*        </Dropdown.Item>*/}
+                    {/*        {*/}
+                    {/*            countries.map((country) => (*/}
+                    {/*                <Dropdown.Item className='text-center mx-auto' onClick={() => { setSelected(country.name.common) }}> {country.name.common}</Dropdown.Item>*/}
+                    {/*            ))*/}
+                    {/*        }*/}
+                    {/*    </DropdownButton>*/}
+                    {/*</Dropdown>*/}
+
+                    <Autocomplete
+                        className='text-light p-3'
+                        id="country-select-demo"
+                        sx={{ width: 300 }}
+                        options={countries}
+                        onInputChange={(event, newInputValue) => {
+                            setSelected(newInputValue);
+                        }}
+                        autoHighlight
+                        getOptionLabel={(option) => option.name.common}
+                        renderOption={(props, option) => (
+                            <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+                                <img
+                                    loading="lazy"
+                                    width="20"
+                                    src={`https://flagcdn.com/w20/${option.cca2.toLowerCase()}.png`}
+                                    srcSet={`https://flagcdn.com/w40/${option.cca2.toLowerCase()}.png 2x`}
+                                    alt=""
+                                />
+                                {option.name.common}
+                            </Box>
+                        )}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Choose a country"
+                                inputProps={{
+                                    ...params.inputProps,
+                                    autoComplete: 'new-password', // disable autocomplete and autofill
+                                }}
+                            />
+                        )}
+                    />
                 </div>
-                <div className='btn btn-primary mt-5 col-4' onClick={() => { selectCountry(); } }> Select Country</div>
+                <div className='btn btn-primary mt-5 col-4' onClick={() => { selectCountry(); }}> Select Country</div>
             </div>
         </>
     );
