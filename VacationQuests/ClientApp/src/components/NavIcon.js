e4aa9520d01f7e5cf72a2e944486984fd66da680
Brennan Phifer <bman5803@gmail.com>
2023-08-14 16:35:19 -0600

    Testing completed merge

--- VacationQuests/ClientApp/src/components/NavIcon.js
+++ VacationQuests/ClientApp/src/components/NavIcon.js
@@ -1,7 +1,7 @@
 ﻿import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import Avatar from '@mui/material/Avatar';
-
+ 
 const NavIcon = () => {
     const [icon, setIcon] = useState('https://i.pinimg.com/736x/dd/f0/11/ddf0110aa19f445687b737679eec9cb2.jpg');
 
